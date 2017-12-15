691895e830f44bb5826bfd048729ed36a6eb431f
Adaliz <adalizitachi11@gmail.com>
2017-12-15 08:15:48 -0600

    modelo y CRUD para citasmedicas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cita_medicas
   resources :doctors
   resources :personas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
